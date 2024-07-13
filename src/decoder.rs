a5d27805ba0f088c2004ac99adcab4181460fc16
Thuthuka Khumalo <thuthuka111@gmail.com>
2024-07-13 12:41:23 +0200

    read peice table from file

--- src/decoder.rs
+++ src/decoder.rs
@@ -5,6 +5,12 @@ use std::{
     io::{self, BufRead, BufReader, Cursor, Read, Seek, SeekFrom},
 };
 
+/// Trait to indicate that a struct can be built from its C representation in bytes
+pub trait CStruct {
+    fn c_size() -> usize;
+    fn from_bytes(bytes: &[u8]) -> Self;
+}
+
 #[allow(non_snake_case)]
 pub struct _STSHI {
     // Style sheet information strucure
@@ -30,11 +36,23 @@ pub struct Fib {
     pub lcbPlcfbtePapx: u32,
     pub fcMin: i32,
     pub fcMac: i32,
+    /* Lengths for the approprate Text Sections */
     pub ccpText: i32,
+    pub ccpFtn: i32,
+    pub ccpHdr: i32,
+    pub ccpMcr: i32,
+    pub ccpAtn: i32,
+    pub ccpEdn: i32,
+    pub ccpTxbx: i32,
+    pub ccpHrdTxbx: i32,
     /// File offset of the STSH (Stylesheet) in the table stream
     pub fcStshf: i32,
     /// Count of bytes of the STSH allocation
     pub lcbStshf: u32,
+    /// Offset in Table Stream of Complex file text portion
+    pub fcClx: i32,
+    /// Length of the fcClx
+    pub lcbClx: i32,
     // Add other FIB fields as needed
 }
 
@@ -100,6 +118,16 @@ pub struct STD {
     // grupx and grupe arrays are omitted as they are variable length
 }
 
+/// Peice Descriptor (PCD)
+#[allow(non_snake_case, unused)]
+#[derive(Debug)]
+pub struct PCD {
+    fNoParaLast: bool,
+    rest_of_bitfield: [bool; 3],
+    fn_val: u16,
+    fc: i32,
+    prm: u16,
+}
 #[allow(non_snake_case, unused)]
 #[derive(Debug)]
 pub struct SHSHI {
@@ -118,12 +146,21 @@ pub struct SHSHI {
     pub styles: Vec<STD>,
 }
 
+/// Represents the piece table (plcfpcd)
+#[allow(non_snake_case, unused)]
+#[derive(Debug)]
+pub struct PLCF<T: CStruct> {
+    rgfc: Vec<i32>,
+    rgstruct: Vec<T>,
+}
+
 #[allow(unused)]
 #[derive(Debug)]
 pub struct WordDocument {
+    cfb: CompoundFile<File>,
     pub fib: Fib,
     pub stylesheet: SHSHI,
-    // pub text: Vec<u8>,
+    pub piece_table: PLCF<PCD>,
 }
 
 // endregion: Structs
@@ -134,16 +171,7 @@ impl WordDocument {
         let mut word_doc_stream = cfb.open_stream("WordDocument")?;
 
         let fib = read_fib(&mut word_doc_stream)?;
-
-        let mut text_buffer = vec![0; fib.ccpText as usize];
-        word_doc_stream.seek(SeekFrom::Start(fib.fcMin as u64))?;
-        word_doc_stream.read_exact(&mut text_buffer)?;
-
-        // Text Section Una
-        println!(
-            "Text Section (raw bytes): {:?}",
-            hex::encode_upper(&text_buffer[..])
-        );
+        println!("{:?}", fib);
 
         // Determine which table stream to use
         let table_stream_name = if !fib.fWhichTblStm {
@@ -152,6 +180,19 @@ impl WordDocument {
             "1Table"
         };
         let mut table_stream = cfb.open_stream(table_stream_name)?;
+
+        // Read the complex part of the document
+        let (_grppls, piece_table) = {
+            let mut complex_buff = vec![0; fib.lcbClx as usize];
+            table_stream.seek(SeekFrom::Start(fib.fcClx as u64))?;
+            table_stream.read_exact(&mut complex_buff)?;
+
+            let mut complex_buff = Cursor::new(complex_buff);
+            let complex_part = read_complex_part(&mut complex_buff)?;
+            println!("{:#?}", complex_part.1);
+
+            complex_part
+        };
 
         // Read the Stylesheet
         let stylesheet = {
@@ -181,7 +222,97 @@ impl WordDocument {
             //     hex::encode_upper(&plcfbte_papx_buffer[..])
             // );
         }
-        Ok(WordDocument { fib, stylesheet })
+        Ok(WordDocument {
+            cfb,
+            fib,
+            stylesheet,
+            piece_table,
+        })
+    }
+
+    pub fn print_cfb_structure(&self) {
+        let entries = self.cfb.walk();
+        for entry in entries {
+            if entry.is_stream() {
+                println!("Stream: {}", entry.name());
+            } else {
+                println!("Storage: {}", entry.name());
+            }
+        }
+    } 
+
+}
+
+/// Read teh text Section of the document
+#[allow(non_snake_case)]
+fn _read_text<R: Read + Seek>(fib: &Fib, reader: &mut R) -> io::Result<()> {
+    if !fib.fComplex {
+        let mut main_text = vec![0; fib.ccpText as usize];
+        reader.seek(SeekFrom::Start(fib.fcMin as u64))?;
+        reader.read_exact(&mut main_text)?;
+
+        let mut footnote = vec![0; fib.ccpFtn as usize];
+        reader.seek(SeekFrom::Start((fib.fcMin + fib.ccpText) as u64))?;
+        reader.read_exact(&mut footnote)?;
+
+        let mut header = vec![0; fib.ccpHdr as usize];
+        reader.seek(SeekFrom::Start(
+            (fib.fcMin + fib.ccpText + fib.ccpFtn) as u64,
+        ))?;
+        reader.read_exact(&mut header)?;
+
+        let mut annotation = vec![0; fib.ccpAtn as usize];
+        reader.seek(SeekFrom::Start(
+            (fib.fcMin + fib.ccpText + fib.ccpFtn + fib.ccpHdr) as u64,
+        ))?;
+        reader.read_exact(&mut annotation)?;
+
+        let endnote_length = fib.fcMin + fib.ccpText + fib.ccpFtn + fib.ccpHdr + fib.ccpAtn
+            - (fib.fcMin + fib.ccpText + fib.ccpFtn + fib.ccpHdr + fib.ccpEdn);
+        let mut endnote = vec![0; endnote_length as usize];
+        reader.seek(SeekFrom::Start(
+            (fib.fcMin + fib.ccpText + fib.ccpFtn + fib.ccpHdr + fib.ccpAtn) as u64,
+        ))?;
+        reader.read_exact(&mut endnote)?;
+
+        let textbox_length =
+            fib.fcMin + fib.ccpText + fib.ccpFtn + fib.ccpHdr + fib.ccpAtn + fib.ccpEdn
+                - (fib.fcMin + fib.ccpText + fib.ccpFtn + fib.ccpHdr + fib.ccpEdn + fib.ccpTxbx);
+        let mut textbox = vec![0; textbox_length as usize];
+        reader.seek(SeekFrom::Start(
+            (fib.fcMin + fib.ccpText + fib.ccpFtn + fib.ccpHdr + fib.ccpAtn + fib.ccpEdn) as u64,
+        ))?;
+        reader.read_exact(&mut textbox)?;
+
+        let header_textbox_length = fib.fcMin
+            + fib.ccpText
+            + fib.ccpFtn
+            + fib.ccpHdr
+            + fib.ccpAtn
+            + fib.ccpEdn
+            + fib.ccpTxbx
+            - (fib.fcMin
+                + fib.ccpText
+                + fib.ccpFtn
+                + fib.ccpHdr
+                + fib.ccpEdn
+                + fib.ccpTxbx
+                + fib.ccpHrdTxbx);
+        let mut header_textbox = vec![0; header_textbox_length as usize];
+        reader.seek(SeekFrom::Start(
+            (fib.fcMin
+                + fib.ccpText
+                + fib.ccpFtn
+                + fib.ccpHdr
+                + fib.ccpAtn
+                + fib.ccpEdn
+                + fib.ccpTxbx) as u64,
+        ))?;
+        reader.read_exact(&mut header_textbox)?;
+
+        todo!();
+    } else {
+        todo!();
     }
 }
 
@@ -207,10 +338,25 @@ fn read_fib<R: Read + Seek>(reader: &mut R) -> io::Result<Fib> {
 
     reader.seek(SeekFrom::Start(0x004C))?;
     let ccpText = reader.read_i32::<LittleEndian>()?;
+    let ccpFtn = reader.read_i32::<LittleEndian>()?;
+    let ccpHdd = reader.read_i32::<LittleEndian>()?;
+    let ccpMcr = reader.read_i32::<LittleEndian>()?;
+    let ccpAtn = reader.read_i32::<LittleEndian>()?;
+    let ccpEdn = reader.read_i32::<LittleEndian>()?;
+    let ccpTxbx = reader.read_i32::<LittleEndian>()?;
+    let ccpHrdTxbx = reader.read_i32::<LittleEndian>()?;
+    println!(
+        "current cursor position: {}",
+        reader.seek(SeekFrom::Current(0))?
+    );
 
     reader.seek(SeekFrom::Start(0x00A2))?;
     let fcStshf = reader.read_i32::<LittleEndian>()?;
     let lcbStshf = reader.read_u32::<LittleEndian>()?;
+
+    reader.seek(SeekFrom::Start(0x01A2))?;
+    let fcClx = reader.read_i32::<LittleEndian>()?;
+    let lcbClx = reader.read_i32::<LittleEndian>()?;
 
     Ok(Fib {
         wIdent,
@@ -222,8 +368,17 @@ fn read_fib<R: Read + Seek>(reader: &mut R) -> io::Result<Fib> {
         fcMin,
         fcMac,
         ccpText,
+        ccpFtn,
+        ccpHdr: ccpHdd,
+        ccpMcr,
+        ccpAtn,
+        ccpEdn,
+        ccpTxbx,
+        ccpHrdTxbx,
         fcStshf,
         lcbStshf,
+        fcClx,
+        lcbClx,
     })
 }
 
@@ -278,6 +433,26 @@ fn read_stylesheet<R: Read + Seek>(stsh_buffer: &mut R) -> io::Result<SHSHI> {
         nVerBuiltInNamesWhenSaved,
         styles,
     })
+}
+
+#[allow(non_snake_case)]
+fn read_complex_part<R: Read + Seek + BufRead>(reader: &mut R) -> io::Result<(Vec<()>, PLCF<PCD>)> {
+    let grpprls = vec![];
+    // NOTE: this currently does not support reading the Grppr byte which starts with one
+    // Hence break if encountered
+    // To rememdy this, if 1 is encountered then you gotta keep going untill the first page byte is a 2
+    let first_byte = reader.read_u8()?; // clxt(2)
+    assert_eq!(first_byte, 2);
+
+    let lcb = reader.read_u32::<LittleEndian>()?;
+    let mut plcfpcd_buff: Vec<u8> = vec![0; lcb as usize];
+    reader.read_exact(&mut plcfpcd_buff)?;
+
+    let plcf = PLCF::<PCD>::from_bytes(&plcfpcd_buff);
+    let remaining_buff = reader.fill_buf()?;
+    assert_eq!(remaining_buff.len(), 0);
+
+    Ok((grpprls, plcf))
 }
 
 #[allow(non_snake_case)]
@@ -363,4 +538,66 @@ fn read_stylesheet_std<R: Read + Seek>(std_buffer: &mut R) -> io::Result<STD> {
         unused: 0,
         xstzName,
     })
+}
+
+impl<T: CStruct> PLCF<T> {
+    fn from_bytes(bytes: &[u8]) -> Self {
+        let cb = bytes.len();
+        #[allow(non_snake_case)]
+        let iMac = (cb - 4) / (4 + T::c_size());
+
+        // Make first array of the Plex
+        let mut rgfc = Vec::with_capacity(iMac + 1);
+        for i in 0..=iMac {
+            let offset = i * 4;
+            let fc = i32::from_le_bytes([
+                bytes[offset],
+                bytes[offset + 1],
+                bytes[offset + 2],
+                bytes[offset + 3],
+            ]);
+            rgfc.push(fc);
+        }
+
+        // Make second array structure[T] of the Plex
+        let mut rgstruct = Vec::with_capacity(iMac);
+        let regstruct_offset = 4 * (iMac + 1);
+        for i in 0..iMac {
+            let offset = regstruct_offset + i * T::c_size();
+            let struct_bytes = &bytes[offset..offset + T::c_size()];
+            let struct_val = T::from_bytes(struct_bytes);
+            rgstruct.push(struct_val);
+        }
+
+        PLCF { rgfc, rgstruct }
+    }
+}
+
+impl CStruct for PCD {
+    fn c_size() -> usize {
+        8
+    }
+
+    #[allow(non_snake_case)]
+    fn from_bytes(bytes: &[u8]) -> Self {
+        assert_eq!(bytes.len(), 8);
+        let bitfield = u16::from_be_bytes([bytes[0], bytes[1]]);
+
+        let fNoParaLast = (bitfield & 0x8000) == 0x8000;
+        let fPaphNil = (bitfield & 0x4000) == 0x4000;
+        let fDirty = (bitfield & 0x2000) == 0x2000;
+        let _ = bitfield & 0x1000;
+        let fn_val = bitfield & 0x0FFF; // Dont know if this correct
+
+        let fc = i32::from_le_bytes([bytes[2], bytes[3], bytes[4], bytes[5]]);
+        let prm = u16::from_be_bytes([bytes[6], bytes[7]]); // Unprocessed
+
+        PCD {
+            fNoParaLast,
+            rest_of_bitfield: [fPaphNil, fDirty, false],
+            fn_val,
+            fc,
+            prm,
+        }
+    }
 }
